5774f5edb781616b0720431753f8bb77eeb76ebc
Haozhen <Haozhen@172.20.20.20>
2017-10-10 12:20:53 -0400

    Final

--- src/main/java/pkgCore/Deck.java
+++ src/main/java/pkgCore/Deck.java
@@ -2,6 +2,12 @@ package pkgCore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.*;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
@@ -11,27 +17,70 @@ public class Deck {
 	private ArrayList<Card> cardsInDeck;
 
 	public Deck() {
+		cardsInDeck = new ArrayList<Card>();
 		for (eSuit eSuit : eSuit.values()) {
 			for (eRank eRank : eRank.values()) {
 				cardsInDeck.add(new Card(eSuit, eRank));
 			}
 		}
+		
 		Collections.shuffle(cardsInDeck);
 	}
 
+
 	//TODO: Fix the Draw method so it throws an exception if the deck is empty
 	public Card Draw() {
+		
 		return cardsInDeck.remove(0);
 	}
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
+	public Card Draw (eSuit eSuit) {
+		for (Card c : this.cardsInDeck) {
+			if (c.geteSuit() == eSuit) {
+				cardsInDeck.remove(c);
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	
+	public Card Draw (eRank eRank) {
+		for (Card c : this.cardsInDeck) {
+			if (c.geteRank() == eRank) {
+				cardsInDeck.remove(c);
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	
+	public int cardCount (eSuit eSuit) {
+		
+		List<Card> cards_eSuit = cardsInDeck.stream().filter(c -> c.geteSuit() == eSuit).collect(Collectors.toCollection(ArrayList::new));
+		return cards_eSuit.size();
+		
+	}
+	
+	
+	public int cardCount (eRank eRank) {
+		
+		List<Card> cards_eRank = cardsInDeck.stream().filter(c -> c.geteRank() == eRank).collect(Collectors.toCollection(ArrayList::new));
+		return cards_eRank.size();
+		
+	}
+	
+	
+	public int Count(Card c) {
+		ArrayList<Card> cards_Rank = cardsInDeck.stream().filter(p -> p.geteRank() == c.geteRank())
+				.collect(Collectors.toCollection(ArrayList::new));
+		ArrayList<Card> cards_Suit = cards_Rank.stream().filter(p -> p.geteSuit() == c.geteSuit())
+				.collect(Collectors.toCollection(ArrayList::new));
+		return cards_Suit.size();
 
-	//TODO: Write a method that will return the number of a given eSuit left in the deck.
-	
-	//TODO: Write a method that will return the number of a given eRank left in the deck.
-	
-	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
+	}
+
 	
 }
